5b1a1aeb373e817627e4a6c76535b7c145ecdb8b
hpanks <hpanks@hotmail.com>
2013-03-18 22:13:31 -0600

    Agregamos la el cutomcomponent ventan y moduficamos el mensaje en versionesaplicacion

--- versiones/src/com/example/versiones/VersionesApplication.java
+++ versiones/src/com/example/versiones/VersionesApplication.java
@@ -4,10 +4,15 @@ import com.vaadin.Application;
 import com.vaadin.ui.*;
 
 public class VersionesApplication extends Application {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public void init() {
-		Window mainWindow = new Window("Versiones Application");
-		Label label = new Label("Hello Vaadin user");
+		Window mainWindow = new Window("Spark");
+		Label label = new Label("Usando git");
 		mainWindow.addComponent(label);
 		setMainWindow(mainWindow);
 	}
